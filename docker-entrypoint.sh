0f9c0e55aa338269b51942a67821af1afadd39b0
diegothuran <diego.thuran@gmail.com>
2018-03-07 16:10:41 -0300

    Deploy configurations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 /manage.py migrate                  # Apply database migrations
-python3 /manage.py collectstatic --noinput  # Collect static files
-python3 /manage.py runserver 0.0.0.0:8000   # Run server
+python /manage.py migrate                  # Apply database migrations
+python /manage.py collectstatic --noinput  # Collect static files
+python /manage.py runserver 0.0.0.0:8000   # Run server
